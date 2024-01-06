0ff3ed1b6a72cd76b0c1ad9cb79d6fe33fc442d2
HOANG THE ANH <therealhoangtheanh@gmail.com>
2024-01-06 13:50:48 +0700

    Change the dashboard

--- src/pages/dashboard/components/preference-shortcut.tsx
+++ src/pages/dashboard/components/preference-shortcut.tsx
@@ -5,7 +5,7 @@ const PrefShortcut = () : JSX.Element => {
         <div className="bg-white rounded-lg p-4 flex flex-col flex-grow">
             <text className="text-black self-center text-2xl">System Preference</text>
             <IoExtensionPuzzle size = {150} className = "fill-indigo-500 self-center"></IoExtensionPuzzle>
-            <button className="btn btn-primary">Modify</button>
+            <button className="btn btn-primary w-7/12 self-center text-white">Modify</button>
         </div>
     )
 }
